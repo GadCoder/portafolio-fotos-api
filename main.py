1829a4fce25c7ba5c5bb26f670f1bced5bebfbd5
german <german.ampuero@unmsm.edu.pe>
2024-06-07 04:03:36 -0500

    fix cors

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def add_cors(app):
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["GET"],
+        allow_methods=["*"],
         allow_headers=["*"],
     )
 
