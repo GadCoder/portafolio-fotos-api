1ae683b006150c90aec8113709aba2a53dae028c
german <german.ampuero@unmsm.edu.pe>
2024-12-21 23:08:15 -0500

    fix credentials validation

--- repository/user.py
+++ repository/user.py
@@ -3,6 +3,8 @@ from settings import settings
 
 
 def authenticate_user(user: str, password: str):
+    print(f"User: {user}, Password: {password}")
+    print(f"Settings user: {settings.user}, Settings password: {settings.password}")
     valid_credentials = (
         str(user) == settings.user and str(password) == settings.password
     )
